658c62fa45227c5d07c580db3391f702f615a187
karubina <karpova.ekaterina19@gmail.com>
2021-10-12 12:15:11 +0300

    commit

---
+++ IgnatevStepan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IgnatevStepan
+//
+//  Created by 35кабинет9 on 08.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
